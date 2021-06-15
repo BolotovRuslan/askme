743da4aa453436aa769395f0438aae60d4078f36
bolotovruslan <lancer11.com@gmail.com>
2021-06-15 13:12:30 +0300

    Change User model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,28 +1,8 @@
-class Question < ApplicationRecord #::Base
+class Question < ApplicationRecord
 
   belongs_to :user
 
-  validates :text, :user, presence:true
-
-  #before_validation :before_validation
-  #after_validation :after_validation
-  #before_save :before_save
-  #before_create :before_create
-  #after_create :after_create
-  #after_save :after_save
-
-  #before_update :before_update
-  #after_update :after_update
-
-  #before_destroy :before_destroy
-  #after_destroy :after_destroy
-
-  #['validation', 'save', 'create', 'update', 'destroy'].each do |action|
-  #  ['before', 'after'].each do |time|
-  #    define_method("#{time}_#{action}") do 
-  #      puts "******> #{time} #{action}"
-  #    end
-  #  end
-  #end
+  validates :text, presence: true
+  validates :text, length: {maximum: 255}
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,40 +1,31 @@
 require 'openssl'
 
-class User < ApplicationRecord #::Base
-
+class User < ApplicationRecord
   ITERATIONS = 20000
   DIGEST = OpenSSL::Digest::SHA256.new
+  USERNAME_FORMAT = /\A\w+\z/
+  EMAIL_FORMAT = /\A[a-z0-9]+\.[a-z0-9]+@[a-z]+\.[a-z]+/i
+
+  attr_accessor :password
 
   has_many :questions
 
-  validates :email, :username, presence: true
-  validates :email, :username, uniqueness: true
-
-  attr_accessor :password
- 
-  validates_presence_of :password, on: :create 
-
-  validates_confirmation_of :password
-
   before_save :encrypt_password
 
-  def encrypt_password
-    if self.password.present?
-      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
-
-      self.password_hash = User.hash_to_string(
-        OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
-      )
-    end
-  end
+  validates :email, :username, presence: true
+  validates :email, :username, uniqueness: true
+  validates :password, presence: true, on: :create
+  validates :password, confirmation: true
+  validates :username, length: { maximum: 40 }
+  validates :username, format: { with: USERNAME_FORMAT}
+  validates :email, format: { with: EMAIL_FORMAT }
 
   def self.hash_to_string(password_hash)
-    password_hash.unpack('H*')[0]
+    password_hash.unpack("H*")[0]
   end
 
   def self.authenticate(email, password)
-    user = find_by(email: email)
-
+    user = find_by(email: email) 
     if user.present? && user.password_hash == User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST))
       user
     else
@@ -42,4 +33,17 @@ class User < ApplicationRecord #::Base
     end
   end
 
+  private
+
+  def encrypt_password
+    if self.password.present?
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length,
+        DIGEST)
+      )
+    end
+  end
+  
 end
