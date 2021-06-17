8a7592b09996c4aeccc3c6f444655bea5dced8eb
bolotovruslan <lancer11.com@gmail.com>
2021-06-17 13:48:26 +0300

    init commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
 class UsersController < ApplicationController
-
-  def show
-    @hello = "Привет, Рус!"  	
+  def index
   end
 
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+    @user = User.new(
+      name: 'Ruslan',
+      username: 'lancer11',
+      avatar_url: 'https://i.vimeocdn.com/portrait/19759422_640x640'
+      )
+
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('16.06.2021')),
+      Question.new(text: 'Гулять идем?', created_at: Date.parse('16.06.2021')),
+    ]
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -24,19 +24,17 @@ class User < ApplicationRecord
   end
 
   def self.authenticate(email, password)
-    user = find_by(email: email)
+    user = find_by(email: email&.downcase)
 
-    return nil if user.blank?
+    return unless user.present?    
 
     hashed_password = User.hash_to_string(
       OpenSSL::PKCS5.pbkdf2_hmac(
         password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST
       )
     )
+    user if user.password_hash == hashed_password
 
-    return user if user.password_hash == hashed_password
-
-    nil
   end
 
   private
