92ac7245f160f097885a91c6edf7018446c64722
bolotovruslan <lancer11.com@gmail.com>
2021-07-01 14:28:37 +0300

    update user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_url, notice: 'Пользователь успешно зарегестрирован!'
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: "Вы успешно зарегистрировались"
     else
       render 'new'
     end
