eae74e8fa50a3c41540087a8b9c839e6288a02e6
bolotovruslan <lancer11.com@gmail.com>
2021-07-02 13:38:57 +0300

    sessions_controller up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
     if user.present?
       session[:user_id] = user.id
-      redirect_to root_url, notice: 'вы успешно залогинились'
+      redirect_to root_path, notice: 'вы успешно залогинились'
     else
       flash.now.alert = 'Неправильный мэйл или пароль'
       render :new   
@@ -16,6 +16,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, notice: 'Вы разлогинились! приходи еще!'
+    redirect_to root_path, notice: 'Вы разлогинились! приходи еще!'
   end
 end
